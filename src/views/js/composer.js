508fcc23d280bf978d64d6cd43db370d67013e55
nSedrickm <nsedrick101@gmail.com>
2020-12-16 16:59:22 +0100

    init questions

---
+++ src/views/js/composer.js
@@ -0,0 +1,6 @@
+// Composer JS
+
+var blk_question_container = document.getElementById("blk_question_container");
+var blk_response_container = document.getElementById("blk_response_container");
+var add_response_button = document.getElementById("add_response_button");
+
