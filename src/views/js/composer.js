e0ac057065486d58ae6c0496cee66a849d405347
nSedrickm <nsedrick101@gmail.com>
2020-12-16 18:03:41 +0100

    map fields with js

--- src/views/js/composer.js
+++ src/views/js/composer.js
@@ -1,6 +1,50 @@
 // Composer JS
 
-var blk_question_container = document.getElementById("blk_question_container");
-var blk_response_container = document.getElementById("blk_response_container");
+var input_question_id = document.getElementById("input_question_id");
+var input_question_text = document.getElementById("input_question_text");
+var select_response_type = document.getElementById("select_response_type");
+var response_value_label = document.getElementById("response_value_label");
+var response_value = document.getElementById("response_value");
 var add_response_button = document.getElementById("add_response_button");
+var blk_question_container = document.getElementById("blk_question_container");
+var blk_responses_container = document.getElementById(
+  "blk_responses_container"
+);
+var blk_response_values = document.getElementById(
+    "blk_response_values"
+  );
+var question_save_button = document.getElementById("question_save_button");
 
+question_save_button.addEventListener("click", () => {
+
+  console.log(`
+   ${input_question_id.value} \n
+   ${input_question_text.value} \n
+   ${select_response_type.value} \n
+   `);
+
+  switch (select_response_type.value) {
+    case "MCQ":
+      // handle
+      break;
+
+    case "multiple":
+      // handle
+      break;
+
+    case "numeric":
+      //handle
+      break;
+    case "short_text":
+      //handle
+      break;
+    case "long_text":
+      //handle
+      break;
+
+    default:
+    //handle
+  }
+});
+
+// ToDo : add handler functions
