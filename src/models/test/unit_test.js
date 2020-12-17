88402769a3be64eaac3dc12404cb40087b9f047d
Promise Fru <promisefru1@gmail.com>
2020-12-17 04:48:05 +0100

    unit_test updated

--- src/models/test/unit_test.js
+++ src/models/test/unit_test.js
@@ -1,9 +1,53 @@
 const assert = require("chai").assert;
 
-describe("mocha test", () => {
-    it("should fail", (done) => {
-        assert.fail();
-        
-        done()
+it('connect to database', (done) => {
+    // done();
+})
+
+describe("vailid input", () => {
+    it("valid email", (done) => {
+        // done()
     })
+
+    it("valid password", (done) => {
+        // done()
+    })
+})
+
+describe("invailid input", () => {
+    it("invalid email", (done) => {
+        // done()
+    })
+
+    it("invalid password", (done) => {
+        // done()
+    })
+})
+
+describe("empty input", () => {
+    it("no email", (done) => {
+        // done()
+    })
+
+    it("no password", (done) => {
+        // done()
+    })
+})
+
+it("harsh password", (done) => {
+    // done();
+})
+
+describe("successfully created", () => {
+    it("return id", (done) => {
+        // done()
+    })
+
+    it("return email used to create", (done) => {
+        // done()
+    })
+})
+
+it("fail to create", () => {
+    // done();
 })
